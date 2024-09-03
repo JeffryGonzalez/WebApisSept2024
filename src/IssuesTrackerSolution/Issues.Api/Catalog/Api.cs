dcea168a11a44bc8d58210fca5dc8c948ad359cf
Jeffry Gonzalez <jeff@hypertheory.com>
2024-09-03 14:33:06 -0400

    some progress

---
+++ src/IssuesTrackerSolution/Issues.Api/Catalog/Api.cs
@@ -0,0 +1,27 @@
+﻿namespace Issues.Api.Catalog;
+
+public class Api : ControllerBase
+{
+
+    [HttpPost("/catalog/{vendor}/software")]
+    public async Task<ActionResult> AddSoftwareToCatalogAsync([FromBody] CreateSoftwareCatalogItemRequest request, string vendor)
+    {
+        if (string.IsNullOrEmpty(request.Name)) // and description
+        {
+            return BadRequest();
+        }
+        if (vendor != "microsoft")
+        {
+            return NotFound();
+        }
+        return Ok(request);
+    }
+}
+
+
+public record CreateSoftwareCatalogItemRequest
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+}
